7ab82f61416cad4927f8f0b1eecea9b5168bd9c7
Reynaldo Gil <gilcu2@gmail.com>
2024-09-04 16:33:50 +0200

    updated CHANGELOG for release 0.5.5

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-VERSION=v$1
-
+VERSION=v${1:-$(kacl-cli current)}
+echo Publising $VERSION
 GOPROXY=proxy.golang.org go list -m github.com/gilcu2/topdiffxml@$VERSION
 goreleaser release --clean
 
