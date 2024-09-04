f31f1e7e6e8a6391e781e30676293a76bb3f6887
Reynaldo Gil <gilcu2@gmail.com>
2024-09-04 05:54:28 +0200

    updated CHANGELOG for release 0.4.6

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,6 +4,7 @@ VERSION=$1
 go mod tidy
 kacl-cli release $VERSION --modify --auto-link
 git commit -a -m "updated CHANGELOG for release $VERSION"
+git push
 git tag v$VERSION
 git push origin v$VERSION
 
